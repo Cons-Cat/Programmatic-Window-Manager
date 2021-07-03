293e5080a4bbb693e14d4867030d186cf942442e
Conscat <wgooch2000@gmail.com>
2021-07-03 19:23:32 -0400

    Fixed aligning border

--- wm.v
+++ wm.v
@@ -37,6 +37,7 @@ enum ScreenStates {
 }
 
 struct Position {
+	// Screen, X, Y, Width, Height
 	states []int
 }
 
@@ -54,20 +55,27 @@ fn main_callback(cmd cli.Command) ? {
 	screen_width := screen_states[ScreenStates.cur_width].int()
 	screen_height := screen_states[ScreenStates.cur_height].int()
 	half_screen_width := screen_width / 2
-	half_screen_height := screen_height / 2
+	// half_screen_height := screen_height / 2
+
+	// For some reason, 'conscat' is the 0th element, so I look at [1].
 	input := os.args_after('focus')[1]
-	// For some reason, 'conscat' is the 0th element.
 	pos := match input {
 		'kitty' {
-			Position{[screen, half_screen_width, 0, half_screen_width, screen_height]}
+			border := get_border('kitty')
+			Position{[screen, half_screen_width - border.states[3], border.states[4],
+				half_screen_width, screen_height - border.states[4]]}
 		}
 		'nyxt' {
-			mut nyxt_x, _ := get_position('nyxt') or {foo()}
-			nyxt_x = if is_focused('nyxt') { if nyxt_x < half_screen_width { half_screen_width } else { 0 }} else { nyxt_x}
+			mut nyxt_x, _ := get_position('nyxt') or { foo() }
+			nyxt_x = if is_focused('nyxt') {
+				if nyxt_x < half_screen_width { half_screen_width } else { 0 }
+			} else {
+				nyxt_x
+			}
 			Position{[screen, nyxt_x, 0, half_screen_width, screen_height]}
 		}
-		'emacs'{
-			Position{[screen,0,0,half_screen_width, screen_height]}
+		'emacs' {
+			Position{[screen, 0, 0, half_screen_width, screen_height]}
 		}
 		else {
 			Position{[0, 0, 0, 600, 300]}
@@ -77,8 +85,8 @@ fn main_callback(cmd cli.Command) ? {
 }
 
 // TODO: I can't figure out a way to out (0,0) in an `or` block.
-fn foo() (int,int){
-	return 0,0
+fn foo() (int, int) {
+	return 0, 0
 }
 
 fn find_or_launch(name string, pos Position) {
@@ -89,7 +97,7 @@ fn find_or_launch(name string, pos Position) {
 
 fn get_position(name string) ?(int, int) {
 	windows_list := os.execute('wmctrl -l -G -x').output.split('\n').filter(it.len > 0)
-	for line in windows_list{
+	for line in windows_list {
 		// TODO: This is a terrible solution lol.
 		parsed_data := line.split(' ').filter(it != '')
 		if parsed_data[6].after('.').to_lower() == name {
@@ -99,11 +107,24 @@ fn get_position(name string) ?(int, int) {
 	return none
 }
 
+fn get_xwin_id(name string) string {
+	return os.execute('xdotool search --class $name').output.before('\n')
+}
+
 fn is_focused(name string) bool {
-	pid := os.execute('xdotool getwindowpid $(xdotool getactivewindow)').output
+	pid := os.execute('xdotool getwindowpid $(xdotool getactivewindow)').output.before('\n')
 	return pid == get_pid(name)
 }
 
-fn get_pid(name string)	string {
-	return os.execute('xdotool getwindowpid $(xdotool search --class $name)').output
+fn get_pid(name string) string {
+	return os.execute('xdotool getwindowpid $(${get_xwin_id(name)})').output
+}
+
+// TODO: Return multiple values instead?
+fn get_border(name string) Position {
+	stats := 'xwininfo -tree -stats -id \'${get_xwin_id(name)}\''
+	println(stats)
+	width := os.execute('$stats | rg \'Relative upper-left X\'').output.after(':  ').before('\n').int()
+	height := os.execute('$stats | rg \'Relative upper-left Y\'').output.after(':  ').before('\n').int()
+	return Position{[0, 0, 0, width, height]}
 }
